698d7bf04bd41fe0b152facf38697f04b0196bce
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-07-29 20:32:04 -0300

    styles and refactors meal item details related code.

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,11 +1,50 @@
-import { Text, View } from "react-native";
+import { Image, Pressable, Text, View, StyleSheet } from "react-native";
 
-function MealItem({ title }) {
+function MealItem({ title, imageUrl, duration, complexity, affordability }) {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable>
+        <View>
+          <Image style={styles.image} source={{ uri: imageUrl }} />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration}m</Text>
+          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+});
